ea0811ed7869075a47b9906cfdf30b0d7f8a9b09
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-04-11 09:22:17 -0700

    add form submit function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,48 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import BlueButton from "../components/blue-button";
 
 export default function page() {
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+
+  const submitAuthorPrompt = (e: any) => {
+    e.preventDefault();
+    console.log(author, title);
+  };
+
   return (
     <div>
-      <section className="max-w-full shadow-2xl rounded-3xl p-20 grid grid-cols-2 gap-36">
+      <section className="max-w-full shadow-2xl rounded-3xl p-20 grid grid-cols-2 gap-24">
+        <Image
+          src="/dashboard.png"
+          alt="Form art"
+          width={400}
+          height={400}
+          priority
+        />
         <div>
           <h3 className="text-4xl pb-20">Chat With Your Favorite Author!</h3>
-          <form className="text-left">
+          <form className="text-left" onSubmit={submitAuthorPrompt}>
             <div className="py-4">
               <label className="text-xl">Enter Your Favorite Author</label>
-              <input className="shadow appearance-none border rounded w-full my-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></input>
+              <input
+                className="shadow appearance-none border rounded w-full my-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={author}
+                onChange={(event) => setAuthor(event.target.value)}
+              />
             </div>
             <label className="text-xl">Enter A Novel</label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-4"></input>
-            <BlueButton text="Submit" />
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-4"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+            <button type="submit">Submit</button>
+            {/* <BlueButton text="Submit" /> */}
           </form>
         </div>
-        <Image src="/form.png" alt="Form art" width={400} height={400} />
       </section>
     </div>
   );
